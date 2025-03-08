3fd98288773faeb977d2380709d080c314b8d601
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-08 08:28:59 -0600

    initialized Node class

---
+++ approach1.js
@@ -0,0 +1,10 @@
+//Recursion - O(n) time and O(n) space
+//* Set the middle element of the array as root
+//Recursively do the same for the left & right half
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
